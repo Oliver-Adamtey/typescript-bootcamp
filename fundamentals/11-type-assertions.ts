ebfc05d7dafe7a4d88e13fed35f278c5cf2e5ec8
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:07:32 +0000

    Create 11-type-assertions.ts

---
+++ fundamentals/11-type-assertions.ts
@@ -0,0 +1,9 @@
+// Type Assertions are mechanisms that allows to directly assign a type to a given variable
+
+
+
+const input = document.getElementById("input-field") as HTMLInputElement;
+
+input.value;
+
+
