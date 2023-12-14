f2c75a3a95298235c53f654785af267c8721eeea
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:05:55 +0000

    arrays enums

---
+++ fundamentals/04-arrays-enums.ts
@@ -0,0 +1,8 @@
+
+
+
+const numbers = [1, 4, 54, 45];
+
+numbers.push(5)
+numbers.length
+console.log(numbers);
---
+++ fundamentals/05-enums.js
@@ -0,0 +1,12 @@
+var CourseType;
+(function (CourseType) {
+    CourseType[CourseType["FREE"] = 0] = "FREE";
+    CourseType[CourseType["PREMIUM"] = 1] = "PREMIUM";
+    CourseType[CourseType["PRIVATE"] = 2] = "PRIVATE";
+    CourseType[CourseType["HIDDEN"] = 3] = "HIDDEN";
+})(CourseType || (CourseType = {}));
+var courseTTU = {
+    title: "TypeScript BootCamp",
+    type: CourseType.HIDDEN
+};
+console.log(courseTTU);
