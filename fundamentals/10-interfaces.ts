a872ed33cc7e1adef28e6747e5f6d245178ea617
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:07:17 +0000

    Create 10-interfaces.ts

---
+++ fundamentals/10-interfaces.ts
@@ -0,0 +1,23 @@
+
+
+
+interface Course {
+    readonly title: string;
+    subtitle: string;
+    lessCount?: number
+ };
+
+
+const course: Course = {
+    title: "TypeScript Bootcamp",
+    subtitle: "Learn the language simple",
+    lessCount: 10
+ };
+
+// course.title = "Hello Ghana";
+
+
+const otherCourse: Course = {
+    title: "TypeScript Bootcamp",
+    subtitle: "Learn the language simple",
+ };
