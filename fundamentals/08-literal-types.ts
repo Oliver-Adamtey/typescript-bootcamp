f0a13ff2d77074c5f41aec10d768910b74963a61
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:07:10 +0000

    Create 08-literal-types.ts

---
+++ fundamentals/08-literal-types.ts
@@ -0,0 +1,17 @@
+
+
+let titleHead = "TypeScript Bootcamp";
+
+
+// literal type which inference the 
+const titlehead = "TypeScript Bootcamp";
+
+
+let pageSize: 10 | 15 | 20 = 10;
+
+pageSize = 10;
+
+
+let courseStatus: "draft" | "published" | "unpublished" | "archived" = "draft";
+
+courseStatus = "archived";
