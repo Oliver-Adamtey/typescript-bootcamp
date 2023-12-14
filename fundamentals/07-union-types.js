88a5d5fd3f53aafd60a33efb1fb16901387b2613
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:06:59 +0000

    union types

---
+++ fundamentals/07-union-types.js
@@ -0,0 +1,9 @@
+// Using the Typescript Union Types - Nullable Variables
+var uniqueIdentifier = 1000;
+uniqueIdentifier = 3000;
+var keys = [100, "Damn"];
+var courseID = 100;
+courseID = null;
+// Strict Null Checks and the strictNullChecks Compiler Flag
+var cID = 100;
+cID = null;
---
+++ fundamentals/07-union-types.ts
@@ -0,0 +1,21 @@
+
+// Using the Typescript Union Types - Nullable Variables
+
+
+let uniqueIdentifier: number | string = 1000;
+
+uniqueIdentifier = 3000;
+
+const keys: (number | string) [] = [100, "Damn"];
+
+let courseID: number | null =100;
+
+courseID =null;
+
+
+
+// Strict Null Checks and the strictNullChecks Compiler Flag
+
+let cID: number | null = 100;
+
+cID = null;
