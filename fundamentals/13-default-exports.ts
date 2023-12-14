e2e47fdeedecae6b7771da1b3d89436651d35417
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-14 08:07:44 +0000

    Create 13-default-exports.ts

---
+++ fundamentals/13-default-exports.ts
@@ -0,0 +1,14 @@
+
+
+export const COURSE_TOTAL = 20;
+
+export const TYPESCRIPT_COURSE = {
+    title: "TypeScript BootCamp",
+    subtitle: "Learn Typescript in a simplified mode",
+    lesssonsCount: 20
+};
+
+export default function printCourse (course) {
+    console.log(`The course tittle is ${course.title}`);
+    
+}
